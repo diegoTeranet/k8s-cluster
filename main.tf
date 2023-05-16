2628f9fe83276a99958fcf8dd85c409ab5476a73
Diego Hernandez <diego.hernandez@teranet.ca>
2023-05-16 20:23:41 +0000

    fixed main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "main" {
 }
 
 module "aks" {
-    source  = "https://tfe.dhernandez.3381802800.workshops.arctiq.ca/TestOrg/aks/azure"
+    source  = "tfe.dhernandez.3381802800.workshops.arctiq.ca/TestOrg/aks/azure"
     version = "1.0.1"
 
     # insert required variables here
