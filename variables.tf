7c3e09edd2469ae060aac14ec2a0b57cd5e58673
Diego Hernandez <diego.hernandez@teranet.ca>
2023-05-16 21:24:59 +0000

    fix test 2

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "cluster_name" { 
-    value = module.aks.kubernetes_cluster_name
+    default = module.aks.kubernetes_cluster_name
     }
 variable "ARM_CLIENT_ID" {}
 variable "ARM_CLIENT_SECRET" {}
